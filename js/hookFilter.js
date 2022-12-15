19afe3445ab754bf2e65342ffdba1197dfb87035
ps-jarvis <ps-jarvis@users.noreply.github.com>
2022-12-15 10:55:18 +0000

    deploy: a120cb5214218c1550df50c1245b5dee6f98318a

--- js/hookFilter.js
+++ js/hookFilter.js
@@ -1,5 +1,5 @@
 $(function() {
-    // '#hooks tr' to be removed when 1.7 doc is deleted
+    // '#hooks dl dt' to be removed when 1.7 doc is deleted
     const allItems = $('#hooks tr, #hookFilter ~ dl dt');
 
     const escapeRegExp = function(string) {
@@ -19,14 +19,33 @@ $(function() {
         });
     };
 
+    /* search must be bi-directional : 
+        - we can type a regex pattern and match against the hook name, 
+        - or our search query can be matched against a dynamic hook name
+        This function is made for this scenario : 
+        we replace the dynamic parts of the hookname with a regex pattern. 
+    */
+    const matchSearchWithDynamicHookName = function(search, hookName){
+        const regex = new RegExp("\<(.*)\>", 'i');
+        const regexedHookName = hookName.replace(regex, "(.*)");
+        if(regexedHookName != hookName){
+            const hookRegex = new RegExp(regexedHookName, 'i');
+            return search.match(hookRegex);
+        }
+        return false;
+    }
+
     const filterList = function(search) {
         let count = 0;
         const regex = new RegExp(escapeRegExp(search), 'i');
-
+        
         allItems.each(function() {
             const $item = $(this);
+            // text is the hookname on 1.7, and all of the table row content on 8 (with location, type, ...)
             const text = $item.text();
-            const matchesSearch = Boolean(text.match(regex));
+            // we need to detect if we are on the 1.7 or 8 hook list - to be removed when 1.7 doc is deleted
+            const hookName = $item.find('a.hookTitle').length > 0 ? $item.find('a.hookTitle').text() : text;
+            const matchesSearch = Boolean(matchSearchWithDynamicHookName(search, hookName) || text.match(regex));
             $item.toggle(matchesSearch);
 
             // to be removed when 1.7 doc is deleted
